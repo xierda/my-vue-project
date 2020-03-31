e125f94fbe0a94e4187b5b5674c8d3b054ec668e
lixiaoqing <lixiaoqing0126@outlook.com>
2020-03-31 14:45:14 +0800

    axios

---
+++ src/utils/request.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
+
+service.interceptors.request.use(
+  config => {
+    return config
+  },
+  error => {
+    console.log(error)
+    return Promise.reject(error)
+  }
+)
+
+service.interceptors.response.use(
+  response => response,
+  error => {
+    console.log('err' + error)
+    return Promise.reject(error)
+  }
+)
+
+export default service
