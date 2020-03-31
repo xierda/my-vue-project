6ba40db653efff3154693fe11233be28e8af50fe
lixiaoqing <lixiaoqing0126@outlook.com>
2020-03-31 13:58:05 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+
+import 'normalize.css/normalize.css'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import '@/styles/index.scss'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
