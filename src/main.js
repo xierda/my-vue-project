bc202cf80f920646dfad53ec17cf6523b89d26cb
lixiaoqing <lixiaoqing0126@outlook.com>
2020-04-29 17:45:51 +0800

    乱七八糟的mock

--- src/main.js
+++ src/main.js
@@ -13,6 +13,13 @@ import store from './store'
 
 import './icons'
 
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
+}
+
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
