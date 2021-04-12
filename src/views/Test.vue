faad39208f5d8df43d943a52dd59e9d85f59560f
imlixiaoqing <imlixiaoqing@gmail.com>
2021-04-12 15:42:44 +0800

    学习.sync修饰符

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="test-wrap">
+
     <!-- 自定义指令 -->
     <!-- <input v-focus type="text">
     <div id="baseexample">
       <p>Scroll down the page</p>
       <p v-pin="100">Stick me 100px from the top of the page</p>
     </div> -->
+
     <!-- 事件修饰符 -->
-    <div v-on:scroll.passive="onScroll">...</div>
+    <!-- <div v-on:scroll.passive="onScroll">...</div> -->
     <!-- <div class="normal" @click.once="outClick">Once</div> -->
     <!-- <a @click.prevent="doThat" href="http://www.baidu.com">百度一下</a> -->
     <!-- <div class="outDiv" @click.self="outClick">
@@ -22,13 +24,28 @@
       outDiv
       <div class="innerDiv" @click.stop="innerClick">innerDiv</div>
     </div> -->
+
+    <!-- 组件.sync修饰符 -->
+    <TestChild :childValue="childValue" :childValue2="childValue2" @update:childValue="childValue = $event"/>
+    <TestChild :childValue.sync="childValue" :childValue2.sync="childValue2"/>
+    <TestChild v-bind.sync="obj"/>
   </div>
 </template>
 
 <script>
+import TestChild from './TestChild'
 export default {
+  components: {
+    TestChild
+  },
   data() {
     return {
+      childValue: 'childValue',
+      childValue2: 'childValue2',
+      obj: {
+        childValue: 'childValue',
+        childValue2: 'childValue2',
+      },
     }
   },
   directives: {
---
+++ src/views/TestChild.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input type="text" v-model="childValue" @focus="onFocus">
+    <input type="text" v-model="childValue2" @focus="onFocus">
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'childValue',
+    'childValue2',
+  ],
+  data() {
+    return {
+      newTitle: 'newTitle',
+      newTitle2: 'newTitle2',
+    }
+  },
+  methods: {
+    onFocus() {
+      this.$emit('update:childValue', this.newTitle)
+      this.$emit('update:childValue2', this.newTitle2)
+    },
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
